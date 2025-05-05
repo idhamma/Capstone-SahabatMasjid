5c793f427769c9942732978f1ca420b30da5c740
sleepy <idhammakbar@gmail.com>
2025-05-05 16:35:28 +0700

    Minor adjustment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation("androidx.credentials:credentials:1.5.0")
     implementation("androidx.credentials:credentials-play-services-auth:1.5.0")
     implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation(libs.androidx.compose.material)
     implementation(libs.androidx.compose.tooling)
     implementation(libs.androidx.compose.tooling.preview)
