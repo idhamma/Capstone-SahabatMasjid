44c844169280611a5470ad4f58062f0b44134098
sleepy <idhammakbar@gmail.com>
2025-05-05 07:55:44 +0700

    fix minor in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,6 @@ plugins {
     id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
 //    id("com.google.gms.google-services") version "4.4.2" apply false
 //    alias(libs.plugins.google.gms.google.services) apply false
-//    alias(libs.plugins.google.gms.google.services) apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    alias(libs.plugins.google.gms.google.services) apply false
+//    id("com.google.gms.google-services") version "4.4.2" apply false
 }
